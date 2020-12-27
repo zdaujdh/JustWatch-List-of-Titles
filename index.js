f55c7beb844b6e8003e84d4123123f0656b1d78c
Nakamuraya Yuki <fourdim53@gmail.com>
2020-12-27 18:22:42 +0900

    wip

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const { log } = require('console');
 const fs = require('fs');
 const puppeteer = require("puppeteer");
 
-function extractItems() {
+function extractLinks() {
   const links = document.querySelectorAll('.title-list-grid__item--link');
   let urls = [];
   for (let link of links) {
-    urls.push(link.getAttribute('href'));
+    urls.push('https://www.justwatch.com' + link.getAttribute('href'));
   }
   return urls;
+}
+
+function extactTitleTexts() {
+  const text = document.querySelector('.title-block h1').textContent;
+  console.log(text);
+  return text;
 }
 
 async function autoScroll(page) {
@@ -24,20 +31,23 @@ async function autoScroll(page) {
           clearInterval(timer);
           resolve();
         }
-      }, 1000);
+      }, 500);
     });
   });
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.setViewport({
     width: 1200,
     height: 5000,
   });
 
-  const contentTypes = ['movie', 'show'];
+  const contentTypes = [
+    'movie',
+    'show'
+  ];
   const genres = [
     'act',
     'ani',
@@ -62,26 +72,33 @@ async function autoScroll(page) {
   const date = new Date();
   const thisYear = date.getFullYear();
 
-  let titles = [];
-  for (let year = 1936; year <= thisYear; year++) {
+  let titleLinks = [];
+  for (let year = 2020; year <= 2020; year++) {
     for (let contentType of contentTypes) {
       for (let genre of genres) {
         await page.goto(
-          `https://www.justwatch.com/jp/動画配信サービス/netflix
-          ?content_type=${contentType}&genres=${genre}
-          &release_year_from=${year}&release_year_until=${year}`
+          `https://www.justwatch.com/jp/動画配信サービス/netflix?content_type=${contentType}&genres=${genre}&release_year_from=${year}&release_year_until=${year}`
         );
 
         await autoScroll(page);
         console.log(year);
-        items = await page.evaluate(extractItems);
+        items = await page.evaluate(extractLinks);
         console.log(items);
-        titles.push(items);
+        titleLinks.push(items);
       }
     }
   }
 
-  fs.writeFileSync('./titles.txt', titles.join('\n') + '\n');
+  let titles = [];
+  for (let titleLink of titleLinks) {
+    console.log(titleLink)
+    await page.goto('https://www.justwatch.com/jp/映画/modern-times')
+    await page.waitForSelector('.title-block')
+    let title = await page.evaluate(extactTitleTexts);
+    titles.push(title);
+  }
+
+  fs.writeFileSync('./titles.html', titles.join('\n') + '\n');
 
   await browser.close();
 })();
